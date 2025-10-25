6a58bc2739cb0ee2a80054460fdc531eb1b2bc05
volkischer beobachter <volkischerbeobachtervolkischer@gmail.com>
2025-10-25 20:24:43 +0300

    Initial Commit

---
+++ QRBarcodeApp/QRBarcodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRBarcodeAppApp.swift
+//  QRBarcodeApp
+//
+//  Created by Andrii Padalka on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct QRBarcodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QRBarcodeAppTests/QRBarcodeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  QRBarcodeAppTests.swift
+//  QRBarcodeAppTests
+//
+//  Created by Andrii Padalka on 25.10.2025.
+//
+
+import Testing
+@testable import QRBarcodeApp
+
+struct QRBarcodeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
